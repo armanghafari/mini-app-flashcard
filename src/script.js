3eedb53c936c13db59c31004384ebb9a6444925d
arman <armanghaffari641.ag@gmail.com>
2025-05-22 17:33:28 +0330

    add card con and edit buttons

--- src/script.js
+++ src/script.js
@@ -22,6 +22,73 @@ closeBtn.addEventListener('click',(hideQuestion = () => {
     if(editBool){
         editBool = false ; 
         submitQueston();
-    }
+    };
 })
 );
+
+cardButton.addEventListener('click',(submitQueston = () => {
+    editBool = false;
+    tempQuestion = question.value.trim();
+    tempAnswer = answer.value.trim();
+    if (!tempQuestion || !tempAnswer) {
+        errorMessage.classList.remove('hide');
+        
+    } else {
+        container.classList.remove("hide");
+        errorMessage.classList.add("hide");
+        viewList();
+        question.value = '';
+        answer.value = '';
+    };
+
+})
+);
+
+function viewList() {
+    let listCard = document.getElementsByClassName("card-list-container");
+    let div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML += `<p class="question-div">${question.value}</p>`;
+    let displayAnswer = document.createElement('p');
+    displayAnswer.classList.add("answer-div" ,"hide");
+    displayAnswer.innerText = answer.value;
+
+    let link = document.createElement('a');
+    link.setAttribute("href","#");
+    link.setAttribute("class","show-hide-btn");
+    link.innerHTML = 'show/hide';
+    link.addEventListener("click",() => {
+        displayAnswer.classList.toggle("hide");
+    });
+    div.appendChild(link);
+    div.appendChild(displayAnswer);
+
+    let buttonCon = document.createElement('div');
+    buttonCon.classList.add("buttons-con");
+    let editButton = document.createElement("button");
+    editButton.setAttribute('class','edit');
+    editButton.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`;
+    editButton.addEventListener("click",() => {
+        editBool = true;
+        modifyElement(editButton , true);
+        addQuestionCard.classList.remove("hide");
+    }); 
+    buttonCon.appendChild(editButton);
+    // 
+    div.appendChild(buttonCon);
+
+    listCard[0].appendChild(div);
+    hideQuestion();
+};
+
+function modifyElement (element , edit = false){
+    let parentDiv = element.parentElement.parentElement;
+    let parentQuestion = parentDiv.querySelector(".question-div").innerText;
+    if (edit) {
+        let parentAns = parentDiv.querySelector(".answer-div").innerText;
+        answer.value = parentAns;
+        question.value = parentQuestion;
+        // disableButtons(true);
+    };
+    parentDiv.remove();
+};
